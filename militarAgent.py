1523901f87259aa0fde6389d7693a0e405ac938a
gabrielpessoa1 <gabrielpessoa1904@gmail.com>
2020-11-28 16:40:11 -0300

    add voidray ability and target closest enemy

--- militarAgent.py
+++ militarAgent.py
@@ -1,8 +1,8 @@
 import sc2
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
-from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR, GATEWAY, \
- CYBERNETICSCORE, STALKER, STARGATE, VOIDRAY
+from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR, GATEWAY, CYBERNETICSCORE, STALKER, STARGATE, VOIDRAY
+from sc2.AbilityId import EFFECT_VOIDRAYPRISMATICALIGNMENT
 import random
 
 class MilitarAgent():
@@ -14,22 +14,28 @@ class MilitarAgent():
         print("Agente militar!")
         await self.train_offensive_units()
         await self.attack()
+        await self.use_ability()
 
     async def attack(self):
         # {UNIT: [n to fight, n to defend]}
         aggressive_units = {STALKER: [15, 5],
                             VOIDRAY: [8, 3]}
-
+        self.targets = self.game.known_enemy_units.filter(lambda unit: unit.can_be_attacked)
 
         for UNIT in aggressive_units:
-            if self.game.units(UNIT).amount > aggressive_units[UNIT][0] and self.game.units(UNIT).amount > aggressive_units[UNIT][1]:
-                for s in self.game.units(UNIT).idle:
-                    await self.game.do(s.attack(self.find_target(self.game.state)))
+            if self.game.units(UNIT).amount > aggressive_units[UNIT][0]:
+                for unit in self.game.units(UNIT).idle:
+                    await self.game.do(unit.attack(self.find_target(self.game.state, unit)))
 
             elif self.game.units(UNIT).amount > aggressive_units[UNIT][1]:
-                if len(self.game.known_enemy_units) > 0:
-                    for s in self.game.units(UNIT).idle:
-                        await self.game.do(s.attack(random.choice(self.game.known_enemy_units)))
+                if len(self.targets) > 0:
+                    for unit in self.game.units(UNIT).idle:
+                        await self.game.do(unit.attack(self.targets.closest_to(unit)))
+
+    async def use_ability(self):
+        for s in self.game.units(VOIDRAY):
+            if s.weapon_cooldown > 0:
+                s(EFFECT_VOIDRAYPRISMATICALIGNMENT)
 
     async def train_offensive_units(self):
         for gw in self.game.units(GATEWAY).ready.noqueue:
@@ -41,10 +47,10 @@ class MilitarAgent():
             if self.game.can_afford(VOIDRAY) and self.game.supply_left > 0:
                 await self.game.do(sg.train(VOIDRAY))
 
-    def find_target(self, state):
-        if len(self.game.known_enemy_units) > 0:
-            return random.choice(self.game.known_enemy_units)
+    def find_target(self, state, unit):
+        if len(self.targets) > 0:
+            return self.targets.closest_to(unit)
         elif len(self.game.known_enemy_structures) > 0:
-            return random.choice(self.game.known_enemy_structures)
+            return self.game.known_enemy_structures.closest_to(unit)
         else:
             return self.game.enemy_start_locations[0]
