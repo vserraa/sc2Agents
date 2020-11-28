e5065e4535ad9597ad859817f4a81d9134d7f6fa
vserraa <vssm@cin.ufpe.br>
2020-11-28 18:43:25 -0300

    improve chronoboost

--- explorationAgent.py
+++ explorationAgent.py
@@ -3,11 +3,14 @@ from sc2.constants import NEXUS, PYLON
 from sc2.ids.ability_id import AbilityId
 from sc2.ids.buff_id import BuffId
 from sc2.units import Unit, Units
+from sc2.constants import NEXUS, PYLON, GATEWAY, ASSIMILATOR, \
+ CYBERNETICSCORE, STALKER, STARGATE, VOIDRAY, FORGE
 
 class ExplorationAgent():
     
     def __init__(self, game_ref):
         self.game = game_ref
+        self.priority = [NEXUS, STARGATE, GATEWAY, FORGE]
 
     async def on_step(self, iteration):
         #print("Agente explorador!")
@@ -22,5 +25,8 @@ class ExplorationAgent():
             
     async def chronoboost(self):
         for nexus in self.game.units(NEXUS).ready:
-            if nexus.energy >= 50 and not nexus.is_idle:
-                await self.game.do(nexus(AbilityId.EFFECT_CHRONOBOOSTENERGYCOST, nexus))
+            for cur_type in self.priority:
+                for building in self.game.units(cur_type).ready:
+                    if not building.is_idle:
+                        if nexus.energy >= 50:
+                            await self.game.do(nexus(AbilityId.EFFECT_CHRONOBOOSTENERGYCOST, nexus))
